f837602d7d451ed0c3b614385584e56a557148f8
Nikolay Markhotskyi <marhotsky@gmail.com>
2024-03-24 23:56:21 +0200

    update

--- controllers/friends/ourFriends.js
+++ controllers/friends/ourFriends.js
@@ -1,11 +1,12 @@
-const path = require('path')
-const { readFile } = require('fs/promises')
-const { catchAsync } = require('../../utils')
+const path = require('path');
+const { readFile } = require('fs/promises');
+const { catchAsync } = require('../../utils');
 
 exports.ourFriends = catchAsync(async (req, res) => {
-  const friendsPath = path.join('externalSources', 'ourFriends.json')
-    
-  const friends = JSON.parse(await readFile(friendsPath))
- 
-  res.status(200).json({friends})
-})
+  const friendsPath = path.join('externalSources', 'ourFriends.json');
+  console.log('friendsPath: ', friendsPath);
+
+  const friends = JSON.parse(await readFile(friendsPath));
+
+  res.status(200).json({ friends });
+});
