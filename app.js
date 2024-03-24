d4f02b10a569e244c48309f66d34a9f337594386
Nikolay Markhotskyi <marhotsky@gmail.com>
2024-03-25 00:36:29 +0200

    update

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 app.use('/api/api-docs', swaggerRouter);
 
