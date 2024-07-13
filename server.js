c7780c2b96b161053b88990630e95335c9ccd9b4
Nikolay <marhotsky@gmail.com>
2024-07-13 16:56:55 +0300

    update

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-require("dotenv").config();
-const { PORT } = process.env;
+const { default: mongoose } = require('mongoose')
+const app = require('./app')
 
-const app = require("./app");
+app.listen(process.env.PORT, () => {
+  console.log("Server running. Use our API on port: 3000")
 
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}!`);
-  });
+  mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('MongoDB successfully connected')
+  }).catch((err)=>{
+    console.log(err)
+  
+    process.exit(1)
+  })
+})
