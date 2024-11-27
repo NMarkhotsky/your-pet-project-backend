b6c04dc5d801b5e5df39e264891cd6d8c9231f36
Nikolay <marhotsky@gmail.com>
2024-11-27 17:10:35 +0200

    update

--- routes/swagerRoutes.js
+++ routes/swagerRoutes.js
@@ -1,27 +1,15 @@
-// const express = require('express');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('../swagger.json');
-
-// const swaggerRouter = express.Router();
-
-// const options = {
-//   explorer: true,
-//   customJsStr: 'console.log("Hello World")',
-// };
-
-// swaggerRouter.use('/', swaggerUi.serve);
-// swaggerRouter.get('/', swaggerUi.setup(swaggerDocument, options));
-
-// module.exports = swaggerRouter;
+const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
-module.exports = function handler(req, res) {
-	if (req.method === 'GET') {
-		return swaggerUi.setup(swaggerDocument, {
-			explorer: true,
-			customJsStr: 'console.log("Hello World")',
-		})(req, res);
-	}
-	res.status(404).send('Not Found');
+const swaggerRouter = express.Router();
+
+const options = {
+	explorer: true,
+	customJsStr: 'console.log("Hello World")',
 };
+
+swaggerRouter.use('/', swaggerUi.serve);
+swaggerRouter.get('/', swaggerUi.setup(swaggerDocument, options));
+
+module.exports = swaggerRouter;
