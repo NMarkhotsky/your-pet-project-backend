c02a91ae44c6026b40bc5b5e5fd3a107f6fd846e
Nikolay <marhotsky@gmail.com>
2024-11-27 16:38:26 +0200

    update

--- controllers/friends/ourFriends.js
+++ controllers/friends/ourFriends.js
@@ -2,11 +2,20 @@ const path = require('path');
 const { readFile } = require('fs/promises');
 const { catchAsync } = require('../../utils');
 
-exports.ourFriends = catchAsync(async (req, res) => {
-	const friendsPath = path.join('public', 'ourFriends.json');
-	console.log('friendsPath: ', friendsPath);
+// exports.ourFriends = catchAsync(async (req, res) => {
+// 	const friendsPath = path.join('public', 'ourFriends.json');
+// 	console.log('friendsPath: ', friendsPath);
 
-	const friends = JSON.parse(await readFile(friendsPath));
+// 	const friends = JSON.parse(await readFile(friendsPath));
+
+// 	res.status(200).json({ friends });
+// });
+
+exports.ourFriends = catchAsync(async (req, res) => {
+	const friendsUrl = 'https://your-pet-project-backend.vercel.app/ourFriends.json';
+
+	const response = await fetch(friendsUrl);
+	const friends = await response.json();
 
 	res.status(200).json({ friends });
 });
